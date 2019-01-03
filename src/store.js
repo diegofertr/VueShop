2de3bdac9145366660ce96719126da48a68261f0
Diego F. Ticona Ramos <dticona@agetic.gob.bo>
2019-01-03 11:46:52 -0400

    Persistencia de datos vuex en IndexedDB

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import localForage from 'localforage'
 import VuexPersistence from 'vuex-persist'
 import products from './modules/products'
 import cart from './modules/cart'
@@ -7,7 +8,8 @@ import cart from './modules/cart'
 Vue.use(Vuex)
 
 const vuexLocal = new VuexPersistence({
-  storage: window.localStorage,
+  /* storage: window.localStorage, */ //Usamos el localStorage para la persistencia de datos
+  storage: localForage, // uso de localforage para la persistencia de datos
   modules: ['cart']
 })
 
